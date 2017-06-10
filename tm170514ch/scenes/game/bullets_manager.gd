3413ec28e4c49e6d5712c1eac0b6d8f852408907
ChanHwi, Choi (aka Ch.) <ccwpc@hanmail.net>
2017-06-10 20:30:00 +0900

    실제로 탄막이 안 그려지던 점 수정

--- tm170514ch/scenes/game/bullets_manager.gd
+++ tm170514ch/scenes/game/bullets_manager.gd
@@ -7,14 +7,16 @@ func _ready():
 
 func _fixed_process(delta):
 	for blt in bullets:
-		blt._fixed_process(delta)
+		blt.process_job(delta)
 		# bullet 도 _fixed_process를 가지지만 노드는 아니므로 직접 호출
 	update() # _draw() 호출
 
 func _draw():
 	for blt in bullets:
-		blt._draw()
+		blt.draw_to(self)
 
 # fire()에 의해 생성된 노드가 extends enemy_bullet 인 경우에 bullets 노드에서 관리
 func add_bullet(enemy_bullet):
-	pass
+	bullets.push_back(enemy_bullet)
+	OS.set_window_title(str(bullets.size()))
+	
