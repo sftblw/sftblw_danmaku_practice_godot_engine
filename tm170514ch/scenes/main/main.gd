fdaa993dc43a6566295d070837335ef086bafe24
ChanHwi, Choi (aka Ch.) <ccwpc@hanmail.net>
2017-07-16 22:53:58 +0900

    내보낸 게임에서 시작이 안 되던 점 수정
    아프로 대소문자를 틀리지 안켓슴니다 ㅡㅡ;;

--- tm170514ch/scenes/main/main.gd
+++ tm170514ch/scenes/main/main.gd
@@ -4,7 +4,7 @@ func _ready():
 	get_node("menu/start").grab_focus()
 	
 func click_start():
-	Scenes.goto_scene("res://scenes/game/game.tscn")
+	Scenes.goto_scene("res://scenes/game/Game.tscn")
 
 func click_settings():
 	pass
